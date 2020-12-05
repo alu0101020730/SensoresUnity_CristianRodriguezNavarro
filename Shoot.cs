32d3bba0cb18c6140b8baa4f8789fccbcbe8533c
Cristian Rodriguez <43813175+alu0101020730@users.noreply.github.com>
2020-12-05 20:19:28 +0000

    Add files via upload

---
+++ Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] Text or;
+    [SerializeField] Text vel;
+    [SerializeField] Text alt;
+
+    void Start()
+    {
+        Input.compass.enabled = true;
+        Input.location.Start();
+
+    }
+
+    void Update()
+    {  
+         or.text = Input.compass.trueHeading.ToString();
+         vel.text = Input.acceleration.x.ToString();
+         alt.text = Input.location.lastData.altitude.ToString();
+    }
+
+
+}
